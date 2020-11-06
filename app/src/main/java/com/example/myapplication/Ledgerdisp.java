2e1c4809954be3c0cd875623f45d91a5e5738540
sou <sou20000129@gmail.com>
2020-11-06 14:49:20 +0900

    プルダウンメニュー作成

--- app/src/main/java/com/example/myapplication/Ledgerdisp.java
+++ app/src/main/java/com/example/myapplication/Ledgerdisp.java
@@ -11,12 +11,24 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Ledgerdisp extends AppCompatActivity {
+
+    // 選択肢
+    private String[] spinnerItems = {"2020年10月 ▼", "2020年11月 ▼"};
+    private TextView textView;
 
     float rainfall[] = {98.8f, 123.8f, 34.6f, 43.9f, 69.4f, 12.5f, 52.8f};
     String monthNames[] = {"家賃", "食費", "趣味", "水道代", "光熱費", "通信費", "その他"};
@@ -28,6 +40,36 @@ public class Ledgerdisp extends AppCompatActivity {
         setContentView(R.layout.activity_ledgerdisp);
 
         setupPieChart();
+        textView = findViewById(R.id.text_view);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        // ArrayAdapter
+        ArrayAdapter<String> adapter
+                = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, spinnerItems);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // spinner に adapter をセット
+        spinner.setAdapter(adapter);
+
+        // リスナーを登録
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            //　アイテムが選択された時
+            @Override
+            public void onItemSelected(AdapterView<?> parent,
+                                       View view, int position, long id) {
+                Spinner spinner = (Spinner)parent;
+                String item = (String)spinner.getSelectedItem();
+                textView.setText(item);
+            }
+
+            //　アイテムが選択されなかった
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
     }
 
 
