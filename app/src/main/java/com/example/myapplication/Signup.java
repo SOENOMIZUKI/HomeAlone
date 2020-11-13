3ee82aaf6e57eba986a4f62ec263e96e08950b5b
shirouzu_junya <aso1801165@gmail.com>
2020-11-13 12:13:58 +0900

    feat: アニメーションをoffにする処理を追加

--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Signup extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //ヒントのアニメーションをoffにする処理
+
+        final TextInputLayout til1 = (TextInputLayout) findViewById(R.id.name_box);
+        final CharSequence hint1 = til1.getHint();
+        til1.setHint(null);
+        til1.getEditText().setHint(hint1);
+        final TextInputLayout til2 = (TextInputLayout) findViewById(R.id.address_box);
+        final CharSequence hint2 = til2.getHint();
+        til2.setHint(null);
+        til2.getEditText().setHint(hint2);
+
     }
 }
