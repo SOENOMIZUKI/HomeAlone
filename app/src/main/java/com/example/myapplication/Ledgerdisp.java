17cd7253b565f9464e90df7fd24e243907825914
sou <sou20000129@gmail.com>
2020-11-30 16:41:21 +0900

    家計簿完成あとUIだけ

--- app/src/main/java/com/example/myapplication/Ledgerdisp.java
+++ app/src/main/java/com/example/myapplication/Ledgerdisp.java
@@ -62,15 +62,19 @@ public class Ledgerdisp extends AppCompatActivity {
     ListView lv;
     SimpleAdapter sAdapter;
     ArrayList<HashMap<String, String>> listData;
-
+    int sum = 0;
+    int[] ia = new int[price.length];
+    int[] en = new int[ia.length];
     // 選択肢
-    private String[] spinnerItems = {"2020/10", "2020/11", "2020/12"};
+    private String[] spinnerItems = {"2020/10", "2020/11", "2020/12", "2021/1", "2021/2", "2021/3", "2021/4", "2021/5", "2021/6",
+            "2021/7", "2021/8", "2021/9"};
     private TextView textView;
 
 
 
 
-    float rainfall[] = {98.8f, 123.8f, 34.6f, 43.9f, 69.4f, 12.5f, 52.8f};
+//    float rainfall[] = {36f,43f,69f,76f,12f,45f,23f};
+    Integer rainfall[] = {en[0],en[1], en[2], en[3],en[4],en[5],en[6]};
     String monthNames[] = {"家賃", "食費", "趣味", "水道代", "光熱費", "通信費", "その他"};
 
 
@@ -118,7 +122,7 @@ public class Ledgerdisp extends AppCompatActivity {
             sqlDB = dbm.getWritableDatabase();
 
 
-
+            //priceリストに金額を入れる
             Kakeibo kakeibo = dbm.selectMoneybook(sqlDB, date.getMonth());
             price[0] = kakeibo.getRent().toString();
             price[1] = kakeibo.getFood_expenses().toString();
@@ -127,6 +131,20 @@ public class Ledgerdisp extends AppCompatActivity {
             price[4] = kakeibo.getCommunication_costs().toString();
             price[5] = kakeibo.getHobby().toString();
             price[6] = kakeibo.getOther().toString();
+
+
+                //priceリストの中身をString型からInt型に変換
+                for (int i = 0; i < price.length; i++) {
+                    ia[i] = Integer.parseInt(price[i]);
+                    rainfall[i] = ia[i];
+                }
+                // 合計金額の保存
+                for(int n = 0; n<ia.length; n++){
+                sum = sum + ia[n];
+                }
+                    TextView txt1 = (TextView) findViewById(R.id.sumtext);
+                    // テキストビューのテキストを設定します
+                    txt1.setText("" + sum);
 
             // ListViewのインスタンスを生成
             ListView listView = findViewById(R.id.listView);
@@ -186,9 +204,9 @@ public class Ledgerdisp extends AppCompatActivity {
                         adapter.setprice(6,kakeibo.getOther().toString());
 
                         adapter.notifyDataSetInvalidated();
-                }catch(ParseException e) {
+                    }catch(ParseException e) {
                         e.printStackTrace();
-                    }
+                        }
                 }
 
                 //　アイテムが選択されなかった
@@ -209,9 +227,9 @@ public class Ledgerdisp extends AppCompatActivity {
                         SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM");
                         Date date = sdFormat.parse(item);
                         intent.putExtra("Month", date);
-                    } catch (ParseException e) {
+                        }  catch (ParseException e) {
                         e.printStackTrace();
-                    }
+                            }
                     startActivity(intent);
                 }
             });
@@ -246,81 +264,6 @@ public class Ledgerdisp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ledgerdisp);
-
-//        // ListViewのインスタンスを生成
-//        ListView listView = findViewById(R.id.listView);
-//
-//        // BaseAdapter を継承したadapterのインスタンスを生成
-//        // レイアウトファイル list_items.xml を
-//        // activity_main.xml に inflate するためにKadapterに引数として渡す
-//        BaseAdapter Kadapter = new kakeiboAdapter(this.getApplicationContext(),
-//                R.layout.kakeibo_list, names, price, photos);
-//
-//        // ListViewにadapterをセット
-//        listView.setAdapter(Kadapter);
-//
-//
-//        //プルダウンメニュー
-//        setupPieChart();
-//
-//        textView =
-//
-//                findViewById(R.id.text_view);
-//
-//        Spinner spinner = findViewById(R.id.spinner);
-//
-//        // ArrayAdapter
-//        ArrayAdapter<String> adapter
-//                = new ArrayAdapter<>(this,
-//                android.R.layout.simple_spinner_item, spinnerItems);
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//
-//        // spinner に adapter をセット
-//        spinner.setAdapter(adapter);
-//
-//        // リスナーを登録
-//        spinner.setOnItemSelectedListener(new
-//
-//          OnItemSelectedListener() {
-//              //　アイテムが選択された時
-//              @Override
-//              public void onItemSelected(AdapterView<?> parent,
-//                                         View view, int position, long id) {
-//                  Spinner spinner = (Spinner) parent;
-//                  String item = (String) spinner.getSelectedItem();
-//                  textView.setText(item);
-//              }
-//
-//              //　アイテムが選択されなかった
-//              public void onNothingSelected(AdapterView<?> parent) {
-//              }
-//          });
-//        //家計簿変更画面遷移
-//        Button changeButton = findViewById(R.id.changebottom);
-//        changeButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                Spinner spinner = (Spinner) findViewById(R.id.spinner);
-////                int idx = spinner.getSelectedItemPosition();
-////                String month = (String)spinner.getSelectedItem();
-//                Intent intent = new Intent(getApplication(),Ledgerchange.class);
-////                intent.putExtra("Month",month);
-//                startActivity(intent);
-//            }
-//        });
-//        //家計簿追加画面遷移
-//        Button inputButton = findViewById(R.id.inputbottom);
-//        inputButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplication(),Ledgerinput.class);
-//                startActivity(intent);
-//            }
-//        });
-
-
-
 
     }
 
--- app/src/main/java/com/example/myapplication/Ledgerinput.java
+++ app/src/main/java/com/example/myapplication/Ledgerinput.java
@@ -60,113 +60,6 @@ public class Ledgerinput extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_ledgerinput);
 
-//            dbm = new DBManager(this);
-//            sqlDB = dbm.getWritableDatabase();
-//
-//            //押された時間のアラーム情報をSQLiteから取得
-//            Kakeibo kakeibo = dbm.selectMoneybook(sqlDB, month.getMonth());
-//            //平日繰り返しがONの時の処理
-//            price[0] = kakeibo.getRent().toString();
-//            price[1] = kakeibo.getFood_expenses().toString();
-//            price[2] = kakeibo.getWater_costs().toString();
-//            price[3] = kakeibo.getUtility_costs().toString();
-//            price[4] = kakeibo.getCommunication_costs().toString();
-//            price[5] = kakeibo.getHobby().toString();
-//            price[6] = kakeibo.getOther().toString();
-//
-//           ListView listView = findViewById(R.id.listView);
-//
-//            kakeiboAdapter adapter = (kakeiboAdapter) listView.getAdapter();
-//            adapter.setprice(0,kakeibo.getRent().toString());
-//            adapter.setprice(1,kakeibo.getFood_expenses().toString());
-//            adapter.setprice(2,kakeibo.getWater_costs().toString());
-//            adapter.setprice(3,kakeibo.getUtility_costs().toString());
-//            adapter.setprice(4,kakeibo.getCommunication_costs().toString());
-//            adapter.setprice(5,kakeibo.getHobby().toString());
-//            adapter.setprice(6,kakeibo.getOther().toString());
-//
-//
-//
-//            // BaseAdapter を継承したadapterのインスタンスを生成
-//            // レイアウトファイル list_items.xml を
-//            // activity_main.xml に inflate するためにKadapterに引数として渡す
-//            BaseAdapter Kadapter = new kakeiboAdapter(this.getApplicationContext(),
-//                    R.layout.kakeibo_list, names, price, photos);
-//
-//            // ListViewにadapterをセット
-//            listView.setAdapter(Kadapter);
-//
-//            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                @Override
-//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                    post = position;
-//                }
-//            });
-//        //入力チェック
-//        final EditText editText = findViewById(R.id.editTextTextPersonName3);
-//
-//        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean flag) {
-//                if(!flag){
-//                    String str = editText.getText().toString().trim();
-//                    if(str.matches("")){
-//                        Toast toast = Toast.makeText(Ledgerinput.this,"入力されていません",Toast.LENGTH_LONG);
-//                        toast.show();
-//                    }
-//                }
-//            }
-//
-//        });
-//
-//
-//        //戻るボタン遷移
-//        Button backButton = findViewById(R.id.back_bottom);
-//        backButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                finish();
-//            }
-//        });
-//        //変更ボタン遷移
-//        Button inputfinish = findViewById(R.id.inputfinish);
-//        inputfinish.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                EditText price = (EditText) findViewById(R.id.editTextTextPersonName3);
-//                //増減値の取得
-//                String input_price = price.getText().toString();
-//                //リスト に紐づいているアダプターを取得
-//                ListView listView = findViewById(R.id.listView);
-//                int pint = Integer.parseInt(input_price);
-//                kakeiboAdapter adapter = (kakeiboAdapter) listView.getAdapter();
-//                //アダプターから選択されているpriceを取得 getpriceを使う
-//
-//                int pri = adapter.getprice(post);
-//                    //priceを増減させる（input_priceを数値に変換する必要がある）
-//                ans = pint + pri;
-//
-//                //変換ごの値でdbを更新する
-//                if(post==0) {
-//                    dbm.onMoneybookRent(sqlDB,month.getMonth(),ans);
-//                }else if(post==1) {
-//                    dbm.onMoneybookFood(sqlDB,month.getMonth(),ans);
-//                }else if(post==2) {
-//                    dbm.onMoneybookWater(sqlDB,month.getMonth(),ans);
-//                }else if(post==3) {
-//                    dbm.onMoneybookUtility(sqlDB,month.getMonth(),ans);
-//                }else if(post==4) {
-//                    dbm.onMoneybookCommunication(sqlDB,month.getMonth(),ans);
-//                }else if(post==5) {
-//                    dbm.onMoneybookHobby(sqlDB,month.getMonth(),ans);
-//                }else if(post==6) {
-//                    dbm.onMoneybookOther(sqlDB,month.getMonth(),ans);
-//                }
-//                //アダプターのsetpriceを使ってアダプターのpriceListの更新をする
-//                String vans = String.valueOf(ans);
-//                adapter.setprice(post, vans);
-//            }
-//        });
     }
 
     @Override
@@ -191,16 +84,6 @@ public class Ledgerinput extends AppCompatActivity {
 
         Log.i("1111111",price.toString());
         ListView listView = findViewById(R.id.listView);
-
-//        kakeiboAdapter adapter = (kakeiboAdapter) listView.getAdapter();
-//        adapter.setprice(0,kakeibo.getRent().toString());
-//        adapter.setprice(1,kakeibo.getFood_expenses().toString());
-//        adapter.setprice(2,kakeibo.getWater_costs().toString());
-//        adapter.setprice(3,kakeibo.getUtility_costs().toString());
-//        adapter.setprice(4,kakeibo.getCommunication_costs().toString());
-//        adapter.setprice(5,kakeibo.getHobby().toString());
-//        adapter.setprice(6,kakeibo.getOther().toString());
-
 
 
         // BaseAdapter を継承したadapterのインスタンスを生成
