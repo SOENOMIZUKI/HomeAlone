5f55d8cc44410444566f503a4ca6160bf79af3ad
mkiUma <beehivetype01@gmail.com>
2020-12-01 12:18:55 +0900

    変更：エラーの出ていた所を最適化

--- app/src/main/java/com/example/myapplication/CaldroidSampleCustomFragment.java
+++ app/src/main/java/com/example/myapplication/CaldroidSampleCustomFragment.java
@@ -3,13 +3,20 @@ package com.example.myapplication;
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidGridAdapter;
 
+import java.util.ArrayList;
+
 public class CaldroidSampleCustomFragment extends CaldroidFragment {
+	ArrayList<String> weather;
+	public CaldroidSampleCustomFragment(ArrayList<String> weather){
+		this.weather = weather;
+	}
 
 	@Override
 	public CaldroidGridAdapter getNewDatesGridAdapter(int month, int year) {
 		// TODO Auto-generated method stub
+
 		return new com.example.myapplication.CaldroidSampleCustomAdapter(getActivity(), month, year,
-				getCaldroidData(), extraData);
+				getCaldroidData(), extraData, weather);
 	}
 
 }
